79abe29d176b7d68bdf139dbf0eb44cd540e2053
0xjaqbek <jaqbek.eth@gmail.com>
2025-07-31 15:11:48 +0200

    ,.,

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/olivia/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    minify: 'esbuild'
-  }
 })
